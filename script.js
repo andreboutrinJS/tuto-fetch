139e181c1ff9bb212f37396c0656528e3fc14388
andreboutrinJS <114679514+andreboutrinJS@users.noreply.github.com>
2023-06-09 20:48:02 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+// CODE
+
+// btn.onclick = () => {
+// //  fetch envois une demande  par url http
+// fetch("https://api.github.com/users" + champ.value)
+// // .then renvois la  1 reponse de :fetch et :json permet de lire le fichier 
+// .then(reponse => reponse.json())
+
+// // .then renvois la 2 réponse 
+// //:data permet de demander a afficher le nom  prenom etc...
+// .then(data => {
+// output.textContent =""
+// output.textContent = `Compte de Mr ${data.user}`
+// // afficher une Image.
+// // 1créer
+// const img = document.createElement("img")
+// // 2 ou se trouve  img
+// img.src = data.avatar_url;
+// output.appendChild(img);
+// })
+// }
+
+
+
